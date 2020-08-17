a5f80d83f95a86a747d17fee349c6d217f07e0e5
Matthieu Minier <minier.matthieu@outlook.com>
2020-08-17 14:57:21 +0200

    migration done

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_17_103626) do
+ActiveRecord::Schema.define(version: 2020_08_17_122846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bikes", force: :cascade do |t|
+    t.string "model"
+    t.string "type"
+    t.string "condition"
+    t.boolean "availability", default: true
+    t.integer "price"
+    t.bigint "users_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["users_id"], name: "index_bikes_on_users_id"
+  end
+
+  create_table "rentals", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.integer "rental_price"
+    t.text "content"
+    t.integer "rating"
+    t.bigint "users_id", null: false
+    t.bigint "bikes_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["bikes_id"], name: "index_rentals_on_bikes_id"
+    t.index ["users_id"], name: "index_rentals_on_users_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +53,7 @@ ActiveRecord::Schema.define(version: 2020_08_17_103626) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bikes", "users", column: "users_id"
+  add_foreign_key "rentals", "bikes", column: "bikes_id"
+  add_foreign_key "rentals", "users", column: "users_id"
 end
