919701d6764253fb1ce7052e6cb97e786c35acc4
Kallyanny Bun <kallyannybun@gmail.com>
2020-08-17 17:03:40 +0200

    add show method and view, and change default value of 'availability' to tru

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,7 +1,7 @@
 class Bike < ApplicationRecord
   belongs_to :user
   has_many :rentals, dependent: :destroy
-
+  has_one_attached :photo
   validates :model, :bike_type, :condition, :price, presence: true
   validates :condition, inclusion: { in: ["fair", "good", "new"], allow_nil: false }
   validates :bike_type, inclusion: { in: ["electric", "flying", "monocycle", "tandem", "folding", "race", "city", "cargo"], allow_nil: false }
