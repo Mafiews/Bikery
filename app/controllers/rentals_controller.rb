e1389780c78568ed79f678019bea860d76b5d2e7
Matthieu Minier <minier.matthieu@outlook.com>
2020-08-18 17:54:37 +0200

    started review

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,7 +1,19 @@
 class RentalsController < ApplicationController
-
   def create
     raise
   end
 
+  def review
+    @review = Review.new(review_params)
+    @review.rental = @rental
+    if @review.save
+      redirect_to rental_path(@rental)
+    else
+      render "review_form"
+    end
+  end
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
 end
