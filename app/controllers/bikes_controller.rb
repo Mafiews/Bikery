c4f87db6585e0b9ebee9e8e14a21c9b305ef08dd
Matthieu Minier <minier.matthieu@outlook.com>
2020-08-18 12:28:06 +0200

    added map on index page

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -2,6 +2,7 @@ class BikesController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
   def index
     @bikes = Bike.all
+    # @api_key = AIzaSyAnyVo9HyzR3Mr_13s4n6FBlDXqymvYeY4
   end
 
   def show
