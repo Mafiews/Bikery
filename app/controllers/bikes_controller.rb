b9652c7a43754cc832ca4feed832a64b0fd5fff9
Yizhu ZONG <agnes.lain@gmail.com>
2020-08-18 12:10:20 +0200

    add new and create for bike

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -7,4 +7,28 @@ class BikesController < ApplicationController
   def show
     @bike = Bike.find(params[:id])
   end
+
+  def new
+    @bike = Bike.new
+    authorize @bike
+  end
+
+  def create
+    @bike = Bike.new(bike_params)
+    @bike.user = current_user
+    authorize @bike
+    @bike.save
+    if @bike.save
+      redirect_to bike_path(@bike)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def bike_params
+    params.require(:bike).permit(:model, :bike_type, :condition, :price, :description, :availability)
+  end
 end
+
--- app/models/bike.rb
+++ app/models/bike.rb
@@ -3,6 +3,7 @@ class Bike < ApplicationRecord
   has_many :rentals, dependent: :destroy
   has_one_attached :photo
   validates :model, :bike_type, :condition, :price, presence: true
+  validates :price, numericality: { :greater_than_or_equal_to => 0 }
   validates :condition, inclusion: { in: ["fair", "good", "new"], allow_nil: false }
   validates :bike_type, inclusion: { in: ["electric", "flying", "monocycle", "tandem", "folding", "race", "city", "cargo"], allow_nil: false }
 end
