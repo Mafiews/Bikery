29d7eccb3ba714c02f7fb2d528fe48fc2da04887
Kallyanny Bun <kallyannybun@gmail.com>
2020-08-18 12:12:23 +0200

    in show view, edit and delete buttons are available for logged in bike owner only

--- app/policies/bike_policy.rb
+++ app/policies/bike_policy.rb
@@ -5,5 +5,23 @@ class BikePolicy < ApplicationPolicy
     end
   end
 
+  def index?
+    true
+  end
 
+  def create?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def update?
+    @record.user == @user
+  end
+
+  def destroy?
+    @record.user == @user
+  end
 end
