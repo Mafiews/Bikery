8b30dcb4e4f78edd8c44b5bc511db1f7a9a72579
Kallyanny Bun <kallyannybun@gmail.com>
2020-08-20 12:58:39 +0200

    index front and map display

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -5,15 +5,20 @@ const mapElement = document.getElementById('map');
 const buildMap = () => {
   mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
   return new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11'
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+  center: [2.353341, 48.856861],
+  zoom: 12
   });
 };
 
 const addMarkersToMap = (map, markers) => {
   markers.forEach((marker) => {
+    const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // info_window
+
     new mapboxgl.Marker()
       .setLngLat([ marker.lng, marker.lat ])
+      .setPopup(popup) // info_window
       .addTo(map);
   });
 };
@@ -21,7 +26,7 @@ const addMarkersToMap = (map, markers) => {
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
+  map.fitBounds(bounds, { padding: 70, maxZoom: 14 });
 };
 
 const initMapbox = () => {
